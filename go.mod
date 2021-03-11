9d4d6afaf1f9d5e7e6af9fc7db7b81847a57a9b8
Alex Tsai <alex.tsai@pdabc.com>
2021-03-12 00:02:45 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/net-byte/opensocks-gui
+
+go 1.16
+
+require (
+	fyne.io/fyne v1.4.3 // indirect
+	github.com/net-byte/opensocks v0.0.0-20210311093229-767f2f101c02 // indirect
+)
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"net"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/theme"
+	"fyne.io/fyne/widget"
+	"github.com/net-byte/opensocks/client"
+	"github.com/net-byte/opensocks/config"
+)
+
+func main() {
+	app := app.New()
+	win := app.NewWindow("openscoks-gui")
+	win.Resize(fyne.NewSize(320, 150))
+	localAddr := widget.NewEntry()
+	localAddr.Text = "127.0.0.1:1081"
+	serverAddr := widget.NewEntry()
+	serverAddr.Text = "example.com:443"
+	username := widget.NewEntry()
+	username.Text = "admin"
+	password := widget.NewPasswordEntry()
+	password.Text = "pass@123456"
+
+	appName := widget.NewLabelWithStyle("OpenSocks v1.0.0", fyne.TextAlignCenter, fyne.TextStyle{})
+	msg := widget.NewLabelWithStyle("", fyne.TextAlignCenter, fyne.TextStyle{})
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			widget.NewFormItem("local addr:", localAddr),
+			widget.NewFormItem("server addr:", serverAddr),
+			widget.NewFormItem("username:", username),
+			widget.NewFormItem("password:", password),
+		},
+	}
+	connectBtn := widget.NewButtonWithIcon("connect", theme.MailSendIcon(), func() {
+		var err error
+		config := config.Config{}
+		config.LocalAddr = localAddr.Text
+		config.ServerAddr = serverAddr.Text
+		config.Username = username.Text
+		config.Password = password.Text
+		if config.LocalAddr == "" || config.ServerAddr == "" {
+			msg.Text = "addr can't be empty!"
+			return
+		}
+		_, err = net.ResolveTCPAddr("tcp", config.LocalAddr)
+		if nil != err {
+			msg.Text = "error local addr format!"
+			return
+		}
+		_, err = net.ResolveTCPAddr("tcp", config.ServerAddr)
+		if nil != err {
+			msg.Text = "error server addr format!"
+			return
+		}
+		go client.Start(config)
+		msg.Text = "connect successfully!"
+
+	})
+	exitBtn := widget.NewButtonWithIcon("exit", theme.CancelIcon(), func() {
+		win.Close()
+	})
+
+	box := widget.NewVBox(appName, form, connectBtn, exitBtn, msg)
+
+	win.SetContent(box)
+	win.ShowAndRun()
+}
