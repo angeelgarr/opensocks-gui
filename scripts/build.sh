3b5c67687d54e11a2570500c26db9a9e459ae982
Alex Tsai <alex.tsai@pdabc.com>
2021-03-28 00:52:19 +0800

    support darwin

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,8 @@
 export GO111MODULE=on
 echo "start building..."
 
-GOOS=linux GOARCH=amd64 go build -o ./bin/opensocks-gui ./main.go
-#GOOS=linux GOARCH=arm64 go build -o ./bin/opensocks-gui-linux-arm64 ./main.go
-#GOOS=darwin GOARCH=amd64 go build -o ./bin/opensocks-gui-macos ./main.go
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -ldflags "-H windowsgui"  -o ./bin/opensocks-gui.exe ./main.go
+GOOS=linux GOARCH=amd64 go build -o ./bin/opensocks-gui-linux-amd64 ./main.go
+GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 CC=o64-clang CXX=o64-clang++ go build -ldflags -s -o ./bin/opensocks-gui-darwin-amd64 ./main.go
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -ldflags "-H windowsgui"  -o ./bin/opensocks-gui-amd64.exe ./main.go
 
 echo "done!"
